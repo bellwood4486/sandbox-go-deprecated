ade934e62615366014722cc1ff25d328d8c7c6d7
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2019-11-08 15:58:20 +0900

    feat: Add price function as handler

--- router/http3/main.go
+++ router/http3/main.go
@@ -16,9 +16,21 @@ func (db database) list(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		_, _ = fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	_, _ = fmt.Fprintf(w, "%f\n", price)
+}
+
 func main() {
 	db := database{"shoes": 50, "socks": 5}
 	mux := http.NewServeMux()
 	mux.Handle("/list", http.HandlerFunc(db.list))
+	mux.Handle("/price", http.HandlerFunc(db.price))
 	log.Fatal(http.ListenAndServe("localhost:8080", mux))
 }
--- router/http4/main.go
+++ router/http4/main.go
@@ -16,8 +16,20 @@ func (db database) list(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		_, _ = fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	_, _ = fmt.Fprintf(w, "%f\n", price)
+}
+
 func main() {
 	db := database{"shoes": 50, "socks": 5}
 	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
