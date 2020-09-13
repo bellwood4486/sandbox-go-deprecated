2c2529e75c6ab00590c923c40816973d55748d4a
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2020-09-13 21:36:31 +0900

    feat: Add a sample of errgroup

---
+++ errgroup/cancel/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func sometimeError(i, j int) error {
+	if i == 5 && j == 3 {
+		return fmt.Errorf("error: goroutine(%d)", i)
+	}
+	return nil
+}
+
+func before() {
+	eg, _ := errgroup.WithContext(context.Background())
+
+	for i := 0; i < 20; i++ {
+		i := i
+		eg.Go(func() error {
+			time.Sleep(100 * time.Millisecond)
+			begin := time.Now()
+			for j := 0; j < i; j++ {
+				select {
+				//case <-ctx.Done():
+				//	fmt.Printf("goroutine(%d) -> canceled\n", i)
+				//	return nil
+				default:
+					if err := sometimeError(i, j); err != nil {
+						fmt.Printf("goroutine(%d) -> error\n", i)
+						return err
+					}
+					time.Sleep(100 * time.Millisecond)
+				}
+			}
+			fmt.Printf("goroutine(%d) -> done %v\n", i, time.Since(begin))
+			return nil
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		fmt.Printf("-----\nerror occured: %v\n", err)
+	}
+}
+
+func after() {
+	eg, ctx := errgroup.WithContext(context.Background())
+
+	for i := 0; i < 20; i++ {
+		i := i
+		eg.Go(func() error {
+			time.Sleep(100 * time.Millisecond)
+			begin := time.Now()
+			for j := 0; j < i; j++ {
+				select {
+				case <-ctx.Done():
+					fmt.Printf("goroutine(%d) -> canceled\n", i)
+					return nil
+				default:
+					if err := sometimeError(i, j); err != nil {
+						fmt.Printf("goroutine(%d) -> error\n", i)
+						return err
+					}
+					time.Sleep(100 * time.Millisecond)
+				}
+			}
+			fmt.Printf("goroutine(%d) -> done %v\n", i, time.Since(begin))
+			return nil
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		fmt.Printf("-----\nerror occured: %v\n", err)
+	}
+}
+
+func main() {
+	start := time.Now()
+	before()
+	fmt.Printf("[before] total time: %v", time.Since(start))
+
+	start = time.Now()
+	after()
+	fmt.Printf("[after] total time: %v", time.Since(start))
+}
