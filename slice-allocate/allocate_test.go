1ed61535ddb836ed6139b26107aab991d36afa17
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2021-02-09 10:24:11 +0900

    feat: Add a sample for memory allocation for slice

--- slice-allocate/allocate_test.go
+++ slice-allocate/allocate_test.go
@@ -1,1 +1,29 @@
 package slice_allocate
+
+import (
+	"testing"
+)
+
+const (
+	_       = iota
+	KiB int = 1 << (10 * iota)
+	MiB
+)
+
+const capacity = 10000
+
+type BigStruct struct {
+	Data [100 * KiB]byte
+}
+
+func BenchmarkAllocateOnce(b *testing.B) {
+	_ = make([]BigStruct, 0, capacity)
+}
+
+func BenchmarkAllocateEverytime(b *testing.B) {
+	s := make([]*BigStruct, 0, capacity)
+	for i := 0; i < capacity; i++ {
+		s = append(s, &BigStruct{})
+	}
+}
+
